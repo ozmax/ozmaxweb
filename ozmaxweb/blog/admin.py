f83f20eb4b229a5bef759b363dee34b5e73dab36
Georgios Tsakalos <ozmax.planet@gmail.com>
2016-04-21 18:24:12 +0300

    update models and admin of blog app

--- ozmaxweb/blog/admin.py
+++ ozmaxweb/blog/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Category, Post
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['nickname', 'user']
+    fields = ['user', 'nickname']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['name']
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'get_user', 'get_author_nick', 'get_categories', ]
+    fields = ['author', 'title', 'slug', 'content', 'categories']
+    prepopulated_fields = {'slug': ('title',)}
+
+    def get_user(self, obj):
+        return obj.author.user.username
+    get_user.short_description = 'User'
+
+    def get_author_nick(self, obj):
+        return obj.author.nickname
+    get_author_nick.short_description = 'wrote as'
+
+    def get_categories(self, obj):
+        return ", ".join(c.name for c in obj.categories.all())
+    get_categories.short_description = 'categories'
--- ozmaxweb/blog/models.py
+++ ozmaxweb/blog/models.py
@@ -4,18 +4,29 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Category(models.Model):
-	name = models.CharField(max_length=255)
-
 class Author(models.Model):
-		pass
+    user = models.ForeignKey(User)
+    nickname = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.nickname
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 
 class Post(models.Model):
-	title = models.CharField(max_length=255)
-	slug = models.SlugField(unique=True, max_length=255)
-	content = models.TextField()
-	author = models.ForeignKey(Author)
-	category = models.ForeignKey(Category)
-	created_at = models.DateTimeField()
-	modified_at = models.DateTimeField()
-
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(Author)
+    categories = models.ManyToManyField(Category)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
