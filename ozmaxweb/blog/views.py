aba96d231f58c6e37df69e2049e77ee951ba128e
Georgios A. Tsakalos <ozmax.planet@gmail.com>
2016-04-28 21:13:12 +0300

    add blog views

--- ozmaxweb/blog/views.py
+++ ozmaxweb/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+def home(request):
+    posts = Post.objects.all().order_by('-created_at')
+    context = {'posts': posts}
+    tmpl = 'blog/home.html'
+    return render(request, tmpl, context)
+
+
+def single_post(request, slug):
+    post = Post.objects.get(slug=slug)
+    context = {'post': post}
+    tmpl = 'blog/single.html'
+    return render(request, tmpl, context)
