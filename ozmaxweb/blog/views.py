c4ef9f697b67950f738a0e3a5b2c6514e1a88f00
Georgios A. Tsakalos <ozmax.planet@gmail.com>
2016-05-05 10:03:35 +0300

    update blog views
    add categories to context

--- ozmaxweb/blog/views.py
+++ ozmaxweb/blog/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 
-from .models import Post
+from .models import Post, Category
 
 
 def home(request):
     posts = Post.objects.all().order_by('-created_at')
-    context = {'posts': posts}
+    categories = Category.objects.all()
+    context = {'posts': posts, 'categories': categories}
     tmpl = 'blog/home.html'
     return render(request, tmpl, context)
 
 
 def single_post(request, slug):
     post = Post.objects.get(slug=slug)
-    context = {'post': post}
+    categories = Category.objects.all()
+    context = {'post': post, 'categories': categories}
     tmpl = 'blog/single.html'
     return render(request, tmpl, context)
