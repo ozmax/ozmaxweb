f84824a3d29ffa63f950f2002a2d6142d58f8559
Georgios A. Tsakalos <ozmax.planet@gmail.com>
2016-05-07 01:24:44 +0300

    add basic pagination to blog

--- ozmaxweb/blog/views.py
+++ ozmaxweb/blog/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post, Category
 
 
 def home(request):
     posts = Post.objects.all().order_by('-created_at')
+    paginator = Paginator(posts, 5)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     categories = Category.objects.all()
     context = {'posts': posts, 'categories': categories}
     tmpl = 'blog/home.html'
