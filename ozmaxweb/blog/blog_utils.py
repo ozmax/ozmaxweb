6c1aa8d5f132633f37cd49d168ce13ba2a6f7cb8
Georgios Tsakalos <ozmax.planet@gmail.com>
2016-05-18 18:10:04 +0300

    implement "more" post functionality
    add tools to blog_utils
    use in views and html templates
    add style

--- ozmaxweb/blog/blog_utils.py
+++ ozmaxweb/blog/blog_utils.py
@@ -2,21 +2,27 @@ import itertools
 import operator
 import calendar
 
+
 def get_archive(posts):
-	dates = [(post.created_at.strftime("%Y"), post.created_at.strftime("%B")) for post in posts]
-	def accumulate(l):
-	    it = itertools.groupby(dates, operator.itemgetter(0))
-	    for key, subiter in it:
-	        yield key, [(key, len(list(group))) for key, group in itertools.groupby([item[1] for item in subiter])]
-	return list(accumulate(dates))
+    dates = [(post.created_at.strftime("%Y"), post.created_at.strftime("%B"))
+             for post in posts]
+
+    def accumulate(l):
+        it = itertools.groupby(dates, operator.itemgetter(0))
+        for key, subiter in it:
+            yield key, [(key, len(list(group))) for key, group in
+                        itertools.groupby([item[1] for item in subiter])]
+    return list(accumulate(dates))
+
 
 def month_num_from_name(month_name):
-	correspondence = {calendar.month_name[i]: i for i in range(1, 13)}
-	try:
-		month = correspondence[month_name]
-	except KeyError:
-		month = None
-	return month
+    correspondence = {calendar.month_name[i]: i for i in range(1, 13)}
+    try:
+        month = correspondence[month_name]
+    except KeyError:
+        month = None
+    return month
+
 
 def sanitize_year(year):
     try:
@@ -33,10 +39,26 @@ def filter_posts(posts, tag=None, year=None, month=None):
         month = month_num_from_name(month)
         year = sanitize_year(year)
         if month and year:
-            posts = posts.filter(created_at__year=year, created_at__month=month)  	
+            posts = posts.filter(created_at__year=year,
+                                 created_at__month=month)
         else:
             posts = posts.none()
     if tag:
         filter_term = tag
         posts = posts.filter(categories__name=tag)
     return posts, filter_term
+
+
+def cut_more(posts):
+    for post in posts:
+        if "&lt;!-- more --&gt;" in post.content:
+            post.content = post.content.split("&lt;!-- more --&gt;")[0]
+            post.has_more = True
+    return posts
+
+
+def clean_more(post):
+    if "&lt;!-- more --&gt;" in post.content:
+        post.content = ''.join(part for part in
+                               post.content.split("&lt;!-- more --&gt;"))
+    return post
--- ozmaxweb/blog/views.py
+++ ozmaxweb/blog/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post, Category
-from .blog_utils import filter_posts, get_archive
+from .blog_utils import filter_posts, get_archive, cut_more, clean_more
 
 
 def home(request, tag=None, year=None, month=None):
     posts = Post.objects.all()
     archive = get_archive(posts)
     posts, filter_term = filter_posts(posts, tag=tag, year=year, month=month)
+
     paginator = Paginator(posts, 5)
     page = request.GET.get('page')
     try:
@@ -17,6 +18,7 @@ def home(request, tag=None, year=None, month=None):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
+    posts = cut_more(posts)
     categories = Category.objects.all()
     context = {'filter_term': filter_term, 'posts': posts,
                'categories': categories, 'archive': archive}
@@ -27,6 +29,7 @@ def home(request, tag=None, year=None, month=None):
 def single_post(request, slug):
     post = get_object_or_404(Post, slug=slug)
     post = Post.objects.get(slug=slug)
+    post = clean_more(post)
     categories = Category.objects.all()
     context = {'post': post, 'categories': categories}
     tmpl = 'blog/single.html'
